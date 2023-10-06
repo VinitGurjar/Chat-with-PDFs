49c15988263ccbfb26d41abf5712412b94809e20
VinitGurjar <vinitfagna@gmail.com>
2023-10-07 02:06:30 +0530

    feat: add text_splitter for dividing text in chunk

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 #For pdf functionality
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 #sidebar contents
 with st.sidebar:
@@ -36,7 +36,17 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        st.write(text)
+        #st.write(text) - Will show the text of pdf on the web
+
+        #Split the text into chunks of 1000 characters(token)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200, 
+            length_function = len
+            ) 
+        chunks = text_splitter.split_text(text=text)
+
+        st.write(chunks)
 
 if __name__ == "__main__":
     main()
